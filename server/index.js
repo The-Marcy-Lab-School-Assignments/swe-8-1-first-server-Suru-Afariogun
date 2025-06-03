5713e58739438e70b97de92566bf81c033df461e
root <root@MARCY-LPT07.localdomain>
2025-06-03 11:43:30 -0400

    Frontend has been fixed, the assignment is complete for real this time

--- server/index.js
+++ server/index.js
@@ -2,17 +2,31 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// The path module is useful for constructing relative filepaths
+const path = require('path');
+
+// the filepath is to the entire assets folder
+const filepath = path.join(__dirname, '../my-app/dist');
+
+// generate middleware using the filepath
+const serveStatic = express.static(filepath);
+
+// Register the serveStatic middleware before the remaining controllers
+app.use(serveStatic);
+
+// other controllers
+
 // === API Endpoints ===
 app.get('/api/joke', (req, res) => {
   res.json({
-    setup: "What do you call a pile of kittens?",
-    punchline: "A meowntain"
+    setup: 'What do you call a pile of kittens?',
+    punchline: 'A meowntain',
   });
 });
 
 app.get('/api/picture', (req, res) => {
   res.json({
-    src: "https://static-cdn.jtvnw.net/jtv_user_pictures/meowntain-profile_banner-71b7a6d0d943dc9e-480.jpeg"
+    src: 'https://static-cdn.jtvnw.net/jtv_user_pictures/meowntain-profile_banner-71b7a6d0d943dc9e-480.jpeg',
   });
 });
 
